61f145a9da2753509f8ff8567f38595a88421049
venkatarangateja <venkatarangateja@gmail.com>
2019-09-09 14:39:20 +0530

    third comiy

--- lambda_function.py
+++ lambda_function.py
@@ -21,36 +21,30 @@ def init_session(accounts,acc_ids):
 
 
 def lambda_handler(event,context):
-	temp_folder = '/tmp/outputs.txt'
-	accounts    = {'613454839298':'arn:aws:iam::613454839298:role/AWSCloudFormationStackSetExecutionRole','006827690841':'arn:aws:iam::006827690841:role/bala_new'}
-	Bucket      = 'cicd-testing-backet'
-	Key         = 'bala/output.txt'
-	stack_name  = 'testing-version-1'
-	s3_client 	= boto3.client('s3')
-	s3_client.download_file(Bucket,Key,temp_folder)
-	pipeline    =  boto3.client('codepipeline')
-	with open(temp_folder) as file_name:
-	    data  		= json.load(file_name)
-    	for acc_ids in data.keys():
-    	    session_token =   init_session(accounts,acc_ids)
-    	    stacks      = get_stacks(session_token)
-    		if stack_name not in get_stacks(session_token):
-    			try:
-    				cft_response = session_token.create_stack(StackName=stack_name,TemplateURL = 'https://'+Bucket+'.s3.amazonaws.com'+str(data[acc_ids]),Parameters=[{'ParameterKey': 'AccountAlias','ParameterValue': 'tejatestingforlambda'},],Capabilities=['CAPABILITY_NAMED_IAM'])
-    				print cft_response
-    				pipe_resp=pipeline.put_job_success_result(jobId=event['CodePipeline.job']['id'])
-    			except Exception, e:
-    				print('the error is:',e)
-    		else:
-    			try:
-    				cft_response = sess_client.update_stack(StackName = stack_name,TemplateURL = 'https://'+Bucket+'.s3.amazonaws.com'+str(data[acc_ids]),Parameters=[{'ParameterKey': 'AccountAlias','ParameterValue': 'tejatestingforlambda'},],Capabilities=['CAPABILITY_NAMED_IAM'])
-					print cft_response
-					pipe_resp=pipeline.put_job_success_result(jobId=event['CodePipeline.job']['id'])
-    			except Exception, e:
-    				print('The error is:',e)
-    			
-		
-			
-
-
-
+    temp_folder = '/tmp/outputs.txt'
+    accounts    = {'613454839298':'arn:aws:iam::613454839298:role/AWSCloudFormationStackSetExecutionRole','006827690841':'arn:aws:iam::006827690841:role/bala_new'}
+    Bucket      = 'cicd-testing-backet'
+    Key         = 'bala/output.txt'
+    stack_name  = 'testing-version-1'
+    s3_client 	= boto3.client('s3')
+    s3_client.download_file(Bucket,Key,temp_folder)
+    pipeline    =  boto3.client('codepipeline')
+    with open(temp_folder) as file_name:
+        data  		= json.load(file_name)
+        for acc_ids in data.keys():
+            session_token =   init_session(accounts,acc_ids)
+            stacks      = get_stacks(session_token)
+            if stack_name not in get_stacks(session_token):
+                try:
+                    cft_response = session_token.create_stack(StackName=stack_name,TemplateURL = 'https://'+Bucket+'.s3.amazonaws.com'+str(data[acc_ids]),Parameters=[{'ParameterKey': 'AccountAlias','ParameterValue': 'tejatestingforlambda'},],Capabilities=['CAPABILITY_NAMED_IAM'])
+                    print cft_response
+                    pipe_resp=pipeline.put_job_success_result(jobId=event['CodePipeline.job']['id'])
+                except Exception, e:
+                    print('the error is:',e)
+            else:
+                try:
+                    cft_response = sess_client.update_stack(StackName = stack_name,TemplateURL = 'https://'+Bucket+'.s3.amazonaws.com'+str(data[acc_ids]),Parameters=[{'ParameterKey': 'AccountAlias','ParameterValue': 'tejatestingforlambda'},],Capabilities=['CAPABILITY_NAMED_IAM'])
+                    print cft_response
+                    pipe_resp=pipeline.put_job_success_result(jobId=event['CodePipeline.job']['id'])
+                except Exception, e:
+                    print('The error is:',e)
